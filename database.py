158dc4bc70ff19ec2f5be7257489a11156e3a813
Parveen C <parru495@gmail.com>
2026-02-27 15:04:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,28 @@
+# ṃysql connection setup
+import mysql.connector
+# database credential
+def get_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="your_password",
+            database="student_db"
+        )
+        return connection
+    except mysql.connector.Error as err:
+        print("Error:", err)
+    
+#create database
 CREATE DATABASE student_db;
 USE student_db;
 
-CREATE TABLE students (
+# create table
+CREATE TABLE students 
+(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    age INT,
+    name VARCHAR(100) Not Null,
+    age INT Not Null,
     course VARCHAR(100),
-    marks FLOAT
-);
+    marks FLOAT Not Null
+)
