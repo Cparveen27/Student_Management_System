622927f50a9247c89fb802d1f8f3965471391baf
Parveen C <parru495@gmail.com>
2026-02-27 16:35:01 +0530

    Refactor database connection and create students1 table
    Removed MySQL connection setup and modified table creation.

--- database.py
+++ database.py
@@ -1,29 +1,14 @@
-# ṃysql connection setup
-import mysql.connector
-# database credential
-def get_connection():
-    try:
-        connection = mysql.connector.connect
-        (
-            host="localhost",
-            user="root",
-            password="your_password",
-            database="student_db"
-        )
-        return connection
-    except mysql.connector.Error as err:
-        print("Error:", err)
-    
 #create database
 CREATE DATABASE student_db;
 USE student_db;
 
 # create table
-CREATE TABLE students 
+CREATE TABLE students1
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) Not Null,
     age INT Not Null,
+    gender VARCHAR(10),
     course VARCHAR(100),
     marks FLOAT Not Null
 )
