ccb45c3f5af520bd66c9b56e7fc2f87c699f9e7e
Parveen C <parru495@gmail.com>
2026-02-28 21:43:51 +0530

    Implement student management menu
    Added a menu-driven interface for student management.

--- Main.py
+++ Main.py
@@ -1,1 +1,35 @@
+from student_operations import *
 
+def show_menu():
+    print("\n" + "="*50)
+    print("     STUDENT MANAGEMENT SYSTEM")
+    print("="*50)
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Update Marks")
+    print("5. Delete Student")
+    print("6. Display Topper")
+    print("7. Exit")
+
+while True:
+    show_menu()
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_student()
+    elif choice == "2":
+        view_students()
+    elif choice == "3":
+        search_by_id()
+    elif choice == "4":
+        update_student()
+    elif choice == "5":
+        delete_student()
+    elif choice == "6":
+        display_topper()
+    elif choice == "7":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice!")
